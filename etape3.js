029b7c2981a3fa9db6a851bccc88dd7a66384f0a
Marianne Nobert <mariannenobert@hotmail.com>
2017-03-21 17:41:05 -0400

    commencement de la troisieme etape

--- etape3.js
+++ etape3.js
@@ -18,39 +18,14 @@ MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) 
   })
 })
 
-//MODIFIER
-app.post('/modifier', (req, res) => {
-var id = req.params.id
- //console.log(id)
- console.log(req.body)
- db.collection('adresse').findOneAndUpdate({"_id": ObjectID(req.body._id)}, {"nom" : req.body.nom, "prenom" : req.body.prenom, "telephone" : req.body.telephone, "ville" : req.body.ville, "codepostal" : req.body.codepostal}, (err, resultat) => {
-
-if (err) return console.log(err)
- res.redirect('/')  // redirige vers la route qui affiche la collection
- })
-})
-
-//SUPPRIMER
-app.get('/detruire/:id', (req, res) => {
- var id = req.params.id
- console.log(id)
- db.collection('adresse')
- .findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
-
-if (err) return console.log(err)
- res.redirect('/')  // redirige vers la route qui affiche la collection
- })
-})
-
-
 app.get('/',  (req, res) => {
    console.log('la route route get / = ' + req.url)
  
-    var cursor = db.collection('adresse').find().toArray(function(err, resultat){
+    var cursor = db.collection('provinces').find().toArray(function(err, resultat){
        if (err) return console.log(err)
     // renders index.ejs
     // affiche le contenu de la BD
-    res.render('index.ejs', {adresse: resultat})
+    res.render('index.ejs', {provinces: resultat})
 
     }) 
     
@@ -61,14 +36,5 @@ app.get('/',  (req, res) => {
 app.get('/formulaire',  (req, res) => {
    console.log('la route  get / = ' + req.url)
    res.sendFile(__dirname + "/public/html/forme.htm")
-})
-
-//AJOUTER
-app.post('/adresse',  (req, res) => {
-  db.collection('adresse').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-      console.log('sauvegarder dans la BD')
-      res.redirect('/')
-    })
 })
 
